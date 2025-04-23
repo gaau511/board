72c67d4243508bf450288dd205b233cd7997cf0a
gaau511 <gaau511@gmail.com>
2025-04-23 16:14:51 +0900

    feat: Add example to each field for swagger utility

--- src/main/java/dev/gaau/board/dto/post/request/PostCreateRequestDto.java
+++ src/main/java/dev/gaau/board/dto/post/request/PostCreateRequestDto.java
@@ -1,5 +1,6 @@
 package dev.gaau.board.dto.post.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 @Getter
@@ -8,7 +9,13 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 public class PostCreateRequestDto {
+
+    @Schema(example = "james")
     private String nickname;
+
+    @Schema(example = "Greeting!")
     private String title;
+
+    @Schema(example = "Hello everyone!")
     private String content;
 }
--- src/main/java/dev/gaau/board/dto/post/request/PostUpdateRequestDto.java
+++ src/main/java/dev/gaau/board/dto/post/request/PostUpdateRequestDto.java
@@ -1,5 +1,6 @@
 package dev.gaau.board.dto.post.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 @Getter
@@ -8,7 +9,13 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 public class PostUpdateRequestDto {
+
+    @Schema(example = "")
     private String nickname;
+
+    @Schema(example = "Title changed")
     private String title;
+
+    @Schema(example = "Content changed")
     private String content;
 }
