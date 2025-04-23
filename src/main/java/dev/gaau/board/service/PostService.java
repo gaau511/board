e4c58804f694875051110396024d4f1a3762cbc4
gaau511 <gaau511@gmail.com>
2025-04-23 14:34:21 +0900

    feat: Implement create post method

--- src/main/java/dev/gaau/board/service/PostService.java
+++ src/main/java/dev/gaau/board/service/PostService.java
@@ -1,10 +1,13 @@
 package dev.gaau.board.service;
 
+import dev.gaau.board.domain.Post;
 import dev.gaau.board.dto.post.request.PostCreateRequestDto;
 import dev.gaau.board.dto.post.request.PostDeleteRequestDto;
 import dev.gaau.board.dto.post.request.PostUpdateRequestDto;
 import dev.gaau.board.dto.post.response.PostAbstractResponseDto;
 import dev.gaau.board.dto.post.response.PostResponseDto;
+import dev.gaau.board.mapper.PostMapper;
+import dev.gaau.board.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,11 +16,19 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class PostService {
-    public PostResponseDto createPost(PostCreateRequestDto request) {
 
+    private final PostRepository postRepository;
+    private final PostMapper postMapper;
+
+    public PostResponseDto createPost(PostCreateRequestDto request) {
+        Post post = postMapper.postCreateRequestDtoToPost(request);
+        Post savedPost = postRepository.save(post);
+
+        return postMapper.postToPostResponseDto(savedPost);
     }
-    public PostResponseDto updatePost(PostUpdateRequestDto request) {}
-    public PostResponseDto deletePost(PostDeleteRequestDto request) {}
-    public PostResponseDto getPostById(Long postId) {}
-    public List<PostAbstractResponseDto> getAllPosts() {};
+
+    public PostResponseDto updatePost(Long postId, PostUpdateRequestDto request) {return null;}
+    public PostResponseDto deletePost(PostDeleteRequestDto request) {return null;}
+    public PostResponseDto getPostById(Long postId) {return null;}
+    public List<PostAbstractResponseDto> getAllPosts() {return null;};
 }
