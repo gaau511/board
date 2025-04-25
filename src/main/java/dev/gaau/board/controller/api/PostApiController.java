721c66fe6c64a6a2c0bfb97ce4aaee9424a47eb0
gaau511 <gaau511@gmail.com>
2025-04-25 16:16:00 +0900

    feat: Separating the postcontroller into API and View
    - REST API is implemented in PostApiController (controller.api)
    - Template views are implemented in PostViewController (controller.view)
    - Clearly separate request paths to /api/board and /view/board

--- src/main/java/dev/gaau/board/controller/api/PostApiController.java
+++ src/main/java/dev/gaau/board/controller/api/PostApiController.java
@@ -1,4 +1,4 @@
-package dev.gaau.board.controller;
+package dev.gaau.board.controller.api;
 
 import dev.gaau.board.dto.post.request.PostCreateRequestDto;
 import dev.gaau.board.dto.post.request.PostUpdateRequestDto;
@@ -7,43 +7,39 @@ import dev.gaau.board.dto.post.response.PostResponseDto;
 import dev.gaau.board.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
-public class PostController {
+@RequestMapping("/api/board")
+public class PostApiController {
 
     private final PostService postService;
 
-    @ResponseBody
-    @PostMapping("/api/write")
+    @PostMapping("/write")
     public ResponseEntity<PostResponseDto> createPost(@RequestBody PostCreateRequestDto request) {
         PostResponseDto postDto = postService.createPost(request);
 
         return ResponseEntity.ok(postDto);
     }
 
-    @ResponseBody
-    @GetMapping("/api")
+    @GetMapping
     public ResponseEntity<List<PostAbstractResponseDto>> viewPostList() {
         List<PostAbstractResponseDto> postDtos = postService.getAllPosts();
 
         return ResponseEntity.ok(postDtos);
     }
 
-    @ResponseBody
-    @GetMapping("/api/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<PostResponseDto> viewPost(@PathVariable("id") Long id) {
         PostResponseDto postDto = postService.getPostById(id);
 
         return ResponseEntity.ok(postDto);
     }
 
-    @ResponseBody
-    @PatchMapping("/api/{id}")
+    @PatchMapping("/{id}")
     public ResponseEntity<PostResponseDto> updatePost(@PathVariable("id") Long id,
                                                       @RequestBody PostUpdateRequestDto request) {
         PostResponseDto postDto = postService.updatePost(id, request);
@@ -51,8 +47,7 @@ public class PostController {
         return ResponseEntity.ok(postDto);
     }
 
-    @ResponseBody
-    @DeleteMapping("/api/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletePost(@PathVariable("id") Long id) {
         postService.deletePost(id);
 
