07d79018af5dfb3fbdea993a803aafe2bae32ea7
gaau511 <gaau511@gmail.com>
2025-04-28 13:25:20 +0900

    feat: Implement view post list page method

---
+++ src/main/java/dev/gaau/board/controller/view/PostViewController.java
@@ -0,0 +1,30 @@
+package dev.gaau.board.controller.view;
+
+import dev.gaau.board.dto.post.request.PostCreateRequestDto;
+import dev.gaau.board.dto.post.request.PostUpdateRequestDto;
+import dev.gaau.board.dto.post.response.PostAbstractResponseDto;
+import dev.gaau.board.dto.post.response.PostResponseDto;
+import dev.gaau.board.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/view/board")
+public class PostViewController {
+
+    private final PostService postService;
+
+    @GetMapping
+    public String viewPostListPage(Model model) {
+        List<PostAbstractResponseDto> postDtos = postService.getAllPosts();
+        model.addAttribute("posts", postDtos);
+
+        return "view/board/list.html";
+    }
+
+}
